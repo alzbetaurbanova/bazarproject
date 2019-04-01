86701ce2d98788f9920e7315672be6028dd03359
Alžbeta Urbanová <alzbet213@outlook.sk>
2019-04-01 13:50:41 +0200

    databaza

--- connect.php
+++ connect.php
@@ -5,16 +5,16 @@ $username = "root";
 $password = "";
 $dbname = "test";
 
-//foreach ($_SERVER as $key => $value) {
-    //if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-     //   continue;
-    //}
-
-   // $connectstr_dbhost = preg_replace("/^."Data Source=(.+?);."$/","\\1", $value);
-   // $connectstr_dbname = preg_replace("/^."Database=(.+?);."$/","\\1", $value);
-   // $connectstr_dbusername = preg_replace("/^."User Id=(.+?);."$/","\\1", $value);
-   // $connectstr_dbpassword = preg_replace("/^."Password=(.+?);."$/","\\1", $value);
-   // }
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $servername = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
